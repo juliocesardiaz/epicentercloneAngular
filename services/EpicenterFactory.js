6339fcb006c3356032fc5ad8db7856e657e1e981
lujxio <jcdiaz92@gmail.com>
2015-09-25 15:17:45 -0700

    finish student/teacher html

--- services/EpicenterFactory.js
+++ services/EpicenterFactory.js
@@ -4,14 +4,16 @@ epicenter.factory('EpicenterFactory', function EpicenterFactory() {
 		{name: "Julio Diaz", signed_in: false},
 		{name: "Juan Mieses", signed_in: false},
 		{name: "Jeremy Wu", signed_in: false},
+		{name: "Ana Weller", signed_in: true},
 		{name: "Angel Mota", signed_in: false},
 		{name: "Brad Mitchell", signed_in: false},
 		{name: "Maria Fernanda", signed_in: false},
 		{name: "Kate Applebaum", signed_in: false},
 	];
 	
-	factory.signInOut = function() {
-		
+	factory.signInOut = function(student, value) {
+		console.log(student);
+		student.signed_in = value;
 	}
 	return factory;
 });
