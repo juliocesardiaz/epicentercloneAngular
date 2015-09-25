9acace0c91cad22d61abd01422d8c0c952cd5458
lujxio <jcdiaz92@gmail.com>
2015-09-25 14:31:42 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var epicenter = angular.module('epicenter', ['ui.router']);
+
+epicenter.config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('home', {
+		url: "",
+		templateUrl: "partials/home.html"
+	});
+	
+	$stateProvider.state('teacher',  {
+		url: "/teacher",
+		templateUrl: "partials/teacher.html",
+		controller: 'EpicenterCtrl'
+	});
+	
+});
